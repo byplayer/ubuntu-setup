f5a88c931f7da9fea4b247d80efa604661843f67
byplayer <byplayer100@gmail.com>
2014-04-19 20:09:53 +0800

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -23,7 +23,8 @@ aptitude install lubuntu-desktop zsh autotools-dev automake libtool \
   chromium-browser \
   ibus-mozc mozc-server mozc-utils-gui \
   exuberant-ctags \
-  emacs24 emacsen-common ibus-el rdtool-elisp xfonts-shinonome python-xlib \
+  emacs24 emacsen-common ibus-el rdtool-elisp emacs-goodies-el \
+  xfonts-shinonome python-xlib \
   libterm-readkey-perl \
   mlocate ssh \
   libsqlite3-dev sqlite3 \
