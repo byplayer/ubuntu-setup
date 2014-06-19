528a1c0965d06b864100ac8c020ba36681aaef49
byplayer <byplayer100@gmail.com>
2014-06-19 23:36:17 +0800

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -21,7 +21,8 @@ aptitude install lubuntu-desktop zsh autotools-dev automake libtool \
   libevent-dev libncurses5-dev zlib1g-dev libbz2-dev libyaml-dev \
   libxml2-dev libxslt1-dev libreadline-dev xclip patch \
   chromium-browser \
-  ibus-mozc mozc-server mozc-utils-gui \
+  mozc-server mozc-utils-gui \
+  emacs-mozc emacs-mozc-bin \
   exuberant-ctags \
   emacs24 emacsen-common ibus-el rdtool-elisp emacs-goodies-el \
   xfonts-shinonome python-xlib \
